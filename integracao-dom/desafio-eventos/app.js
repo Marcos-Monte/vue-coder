f4d9d1018b69fb5cf0be589539d58ef7e1dcc34f
Marcos-Monte <marcos.monte.junior@gmail.com>
2024-11-18 12:27:24 -0300

    feat(integracao-dom/desafio-eventos): Código com o segundo desafio do curso. Eventos

---
+++ integracao-dom/desafio-eventos/app.js
@@ -0,0 +1,22 @@
+/* Instancia Vue */
+new Vue({
+    /* Elemento que será monitorado */
+    el: '#desafio',
+    /* Atributos da Instancia */
+    data:{
+        valor: 'Vazio'
+    },
+    /* Métodos e Funções da Instancia */
+    methods: {
+        /* Exibir alerta ao clicar no botão */
+        exibirAlerta(){
+            alert('Exercício 1')
+        },
+        /* Atribuir valor digitado no input no atributo 'valor' */
+        /* Será chamado independente das indicações do 'keydown' */
+        armazenarValor(event){
+            this.valor = event.target.value
+        },
+
+    }
+})
