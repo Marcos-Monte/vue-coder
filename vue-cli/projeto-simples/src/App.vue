365380b6faf9b203b87aaa95110af6dd3c3510ed
Marcos-Monte <marcos.monte.junior@gmail.com>
2024-12-03 16:38:54 -0300

    feat: aulas de desenvolvimento usando componentes no Vue Client

--- vue-cli/projeto-simples/src/App.vue
+++ vue-cli/projeto-simples/src/App.vue
@@ -1,26 +1,88 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Template Html -> Aparentemente, necessário em todos os componentes -->
+<template id="App">
+  <!-- :class -> deve ser usado apenas em elementos HTML (template não funciona) -->
+  <div class="main" :class="estilo ? 'light' : 'dark'">
+    <!-- Componente Importado -->
+    <Hello 
+      msg="Componente Importado"
+    />
+
+    <h1> Estilo {{ titulo }}</h1>
+
+    <!-- Ao clicar muda a cor do fundo -->
+    <!-- Necessário a 'diretiva' de 'binding'. Com isso não se usa 'parenteses' na função -->
+    <MeuBotao :funcao="alterar" texto="Dark" />
+    <MeuBotao v-bind:funcao="voltar" texto="Light" />
+
+  </div>
+
 </template>
 
+<!-- Componente Vue -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* Importando Componente Externo */
+  import Hello from './components/HelloWorld.vue';
+  import MeuBotao from './components/Botao.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    /* Adicionando Componentes Externos */
+    components: {
+
+      Hello,
+      MeuBotao,
+
+    },
+
+    data: function () {
+      /* Dados retornados por uma função, garante que cada componente tenha seu proprio conjunto de dados isolados */
+      return {
+        
+        titulo: "Light",
+        estilo: true,
+      };
+
+    },
+
+    /* Métodos da Aplicação */
+    methods: {
+      alterar() {
+        this.estilo = false;
+        this.titulo = 'Dark'
+      },
+
+      voltar(){
+        this.estilo = true;
+        this.titulo = 'Light'
+      }
+    },
+  };
 </script>
 
+<!-- /* Css */ -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  .main{
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    gap: .5rem;
+  }
+
+  button{
+    width: 12rem;
+
+  }
+
+  /* Classes de Estilos */
+  .light {
+    background-color: #ffffff;
+    color: red
+  }
+
+  .dark {
+    background-color: #103434;
+  }
 </style>
---
+++ vue-cli/projeto-simples/src/components/Botao.vue
@@ -0,0 +1,31 @@
+<!-- Template Html -> Aparentemente, necessário em todos os componentes -->
+<template>
+
+    <!-- Componente 'button' recebe função e texto, como propriedades -->
+    <button @click="funcao">
+        {{ texto }}
+    </button>
+
+</template>
+
+<script>
+
+    /* Objeto Padrão da aplicação */
+    export default {
+        /* Nomear o componente no Objeto / Instancia é Obrigatorio */
+        name: "MeuBotao",
+        /* Defininco as 'props', recebidas pelo Componente */
+        props: {
+            /* Função recebe um valor do 'tipo Funcion' e não é obrigatoria */
+            funcao: {
+            type: Function,
+            required: false,
+            },
+            /* Texto recebe um valor do 'tipo String' e é obrigatoria */
+            texto: {
+            type: String,
+            required: true,
+            },
+        },
+    };
+</script>
