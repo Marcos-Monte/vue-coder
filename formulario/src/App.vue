03e62a3ca45791987b0a9e90e2c25c9f283ba73c
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-01-13 20:39:19 -0300

    docs: Comentarios no formulario de exemplo do módulo de forms

--- formulario/src/App.vue
+++ formulario/src/App.vue
@@ -1,80 +1,124 @@
 <template>
+	<!-- Container Pai, funciona como o 'body' -->
 	<div id="app">
+
 		<h1>Registrar Reclamação</h1>
+
+		<!-- Container flex que envolve os dois 'forms' -->
 		<div class="conteudo">
+
+			<!-- Painel de inserção de dados pelo Usuario (form)-->
 			<form class="painel">
+
 				<div class="cabecalho">Formulário</div>
+
 				<Rotulo nome="E-mail">
-					<input type="text">
+					<input type="text" v-model="email" required /> <!-- V-model faz o Two-way data binding -->
 				</Rotulo>
+
 				<Rotulo nome="Senha">
-					<input type="password">
+					<input type="password" v-model="senha" required /> <!-- V-model faz o Two-way data binding -->
 				</Rotulo>
+
 				<Rotulo nome="Idade">
-					<input type="number">
+					<input type="number" v-model="idade" required /> <!-- V-model faz o Two-way data binding -->
 				</Rotulo>
+
 				<Rotulo nome="Mensagem">
-					<textarea name="" cols="30" rows="5"></textarea>
+					<textarea name="" cols="30" rows="5" v-model="msg" required></textarea> <!-- V-model faz o Two-way data binding -->
 				</Rotulo>
+
 				<Rotulo nome="Características do Problema">
 					<span class="mr-4"><input type="checkbox" value="reproduzivel"> Reproduzível</span>
 					<span><input type="checkbox" value="intermitente"> Intermitente</span>
 				</Rotulo>
+
 				<Rotulo nome="Qual produto?">
 					<span class="mr-4"><input type="radio"> Web</span>
 					<span class="mr-4"><input type="radio"> Mobile</span>
 					<span><input type="radio"> Outro</span>
 				</Rotulo>
+
 				<Rotulo nome="Prioridade">
 					<select name="" id="">
 						<option></option>
 					</select>
 				</Rotulo>
+
 				<Rotulo nome="Primeira Reclamação?">
 					<Escolha />
 				</Rotulo>
+
 				<hr>
+				
 				<button>Enviar</button>
 			</form>
+
+			<!-- Painel de Visualização dos dados armazenados. -->
 			<div class="painel">
+
 				<div class="cabecalho">Resultado</div>
+
 				<Rotulo nome="E-mail">
-					<span>???</span>
+					<span>{{ email }}</span>
 				</Rotulo>
+
 				<Rotulo nome="Senha">
-					<span>???</span>
+					<span>{{ senha }}</span>
 				</Rotulo>
+
 				<Rotulo nome="Idade">
-					<span>???</span>
+					<span>{{ idade }}</span>
 				</Rotulo>
+
 				<Rotulo nome="Mensagem">
-					<span>???</span>
+					<span>{{ msg }}</span>
 				</Rotulo>
+
 				<Rotulo nome="Marque as Opções">
 					<span>???</span>
 				</Rotulo>
+
 				<Rotulo nome="Qual produto?">
 					<span>???</span>
 				</Rotulo>
+
 				<Rotulo nome="Prioridade">
 					<span>???</span>
 				</Rotulo>
+
 				<Rotulo nome="Primeira Reclamação?">
 					<span>???</span>
 				</Rotulo>
+
 			</div>
+
 		</div>
+
 	</div>
+
 </template>
 
 <script>
-import Rotulo from './components/Rotulo.vue'
-import Escolha from './components/Escolha.vue'
 
-export default {
-	name: 'app',
-	components: { Rotulo, Escolha }
-}
+/* Imports dos Componentes */
+import Escolha from './components/Escolha.vue';
+import Rotulo from './components/Rotulo.vue';
+
+	export default {
+		name: 'app',
+		/* Registrando componentes */
+		components: { Rotulo, Escolha },
+
+		data(){
+			return {
+				email: '',
+				senha: '',
+				idade: '',
+				msg: '',
+			}
+		}
+	}
 </script>
 
 <style>
