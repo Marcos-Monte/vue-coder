7bb822d4fda1db4fa6c68963f9c6d8c6775a6aca
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-01-21 10:21:06 -0300

    feat: restante do módulo de Diretivas Personalizadas incluíndo o Desafio proposto

---
+++ diretivas/diretivas-desafio/README.md
@@ -0,0 +1,10 @@
+# Desafio sobre Diretivas
+
+## Exercício:
+- Escreva uma diretiva que funcione com o v-on (escute eventos)
+
+### Observações:
+- Funcionar como 'v-on', significa que a diretiva vai atender 'eventos' 
+- Deve receber uma função que será executada mediante o chamamento do 'evento'
+- Podemos fazer uma que atende a um evento de 'click', primeiramente, e depois para atender a eventos de mouse (mouseover, mouseleave, etc)
+- Dica: Criar uma diretiva 'v-quando' em uma tag 'button' -> 'v-quando:click="função()"'
---
+++ diretivas/diretivas-desafio/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+
+	<div id="app">
+
+		<h1>Diretivas (Desafio)</h1>
+
+		<hr>
+
+		<!-- Diretiva: verifica a emissão do evento 'click' e aciona a função -->
+    <button v-quando:click="acaoClick">
+      Executar
+    </button>
+
+    <!-- Diretiva: verifica a emissão dos eventos 'mouseenter e mouseleave' e aciona a função -->
+    <div 
+      v-quando:mouseenter="mouseEnter"
+      v-quando:mouseleave="mouseLeave"
+    >
+      <p>Teste Mouse Event!</p>
+    </div>
+
+	</div>
+
+</template>
+
+<script>
+
+  export default {
+
+    // Registrando Diretiva Personalizada
+    directives: {
+
+      // Nome da Diretiva --> v-quando
+      'quando': {
+
+        // Montando a Diretiva --> el (elemento), binding (interações)
+        mounted(el, binding){
+
+          // Argumento será o 'evento de click', similar ao 'v-on:click'. Podemos usar qualquer tipo de 'evento padrão'
+          const argumento = binding.arg;
+          // Função será recebica como valor
+          const funcao = binding.value;
+
+          // Adicionando um 'escutador' de eventos. 
+          el.addEventListener(argumento, funcao)
+
+        }
+
+      }
+
+    },
+
+    methods: {
+
+      // Função inicia uma caixa de alerta 
+      acaoClick(){
+        alert('Ação Executada!')
+      },
+
+      // Função mostra no terminal a mensagem
+      mouseEnter(){
+        console.log('Dentro da Div')
+      },
+
+      // Função mostra no terminal a mensagem
+      mouseLeave(){
+        console.log('Fora da Div')
+      }
+
+    }
+
+  }
+
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+button {
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+</style>
