a1c2b5a55b34b9279b0f829a1f377de371f0cc6a
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-01-22 19:38:06 -0300

    feat: modulo de integração do vue com cliente HTTP Axios

--- servidor-http/http-exercicios/src/App.vue
+++ servidor-http/http-exercicios/src/App.vue
@@ -4,16 +4,27 @@
 
 		<h2>HTTP com Axios</h2>
 
+		<!-- Componente Bootstrap-vue que mostra um alerta com uma mensagem após uma ação executada -->
+		<b-alert 
+			:show="500"
+			v-for="mensagem in mensagens" 
+			:key="mensagem.texto"
+			:variant="mensagem.tipo"
+		>
+			{{ mensagem.texto }}
+		</b-alert>
+
 		<!-- Componente do Boostrap-Vue -->
-		<b-card>
+		<b-card :class="classe? 'registrando': 'alterando'">
 			<!-- Funciona como o Fieldset -->
 			<b-form-group label="Nome: ">
-				<!-- Input -->
+				<!-- Input da propriedade 'nome'-->
 				<b-form-input 
 					type="text" 
 					size="lg"
 					placeholder="Informe o Nome"
 					v-model="usuario.nome"
+					required
 				/>
 				
 			</b-form-group>
@@ -21,7 +32,7 @@
 			<!-- Funciona como o Fieldset -->
 			<b-form-group label="Email: ">
 			
-			<!-- Input -->
+			<!-- Input da propriedade 'email'-->
 			<b-form-input 
 				type="email" 
 				size="lg"
@@ -33,21 +44,13 @@
 
 			<hr>
 
+			<!-- Aciona o método de 'salvamento' de novo registro no Banco de Dados -->
 			<b-button 
 				size="lg"
 				variant="primary"
-				@click="salvar()"
+				@click="postEPutDados()"
 			>
 				Salvar
-			</b-button>
-
-			<b-button 
-				size="lg"
-				variant="success"
-				class="ml-5"
-				@click="mostrar()"
-			>
-				Mostrar Usuarios
 			</b-button>
 
 		</b-card>
@@ -58,13 +61,45 @@
 
 		<b-list-group>
 
-			<b-list-group-item v-for="(usuario, index) in usuarios" :key="index">
+			<!-- Se a lista de Usuários não estiver vazia, mostrar o elemento. -->
+			<template v-if="usuarios">
 
-				<strong>Nome: {{ usuario.nome }} </strong> <br>
-				<strong>Email: {{ usuario.email }}</strong> <br>
-				<strong>ID: {{ index }}</strong>
+				<!-- Percorrer o Array 'usuarios' -->
+				<b-list-group-item v-for="(usuario, index) in usuarios" :key="index">
+					<!-- Renderizando os dados de cada 'usuario' de dentro de 'usuarios' -->
+					<strong>Nome: {{ usuario.nome }} </strong> <br>
+					<strong>Email: {{ usuario.email }}</strong> <br>
+					<strong>ID: {{ index }}</strong>
+					
+					<!-- Renderiza uma Divisão -->
+					<hr>
 
-			</b-list-group-item>
+					<!-- Ao clicar, insere o valor 'index' na função 'carregar' que vai setar na variavel 'id' em data -->
+					<b-button
+						variant="warning"
+						size="lg"
+						@click="carregar(index)"
+					>
+						Alterar
+					</b-button>
+
+					<b-button
+						variant="danger"
+						size="lg"
+						class="ml-2"
+						@click="deleteDado(index)"
+					>
+						Excluir
+					</b-button>
+					
+				</b-list-group-item>
+
+			</template>
+
+			<!-- Se a lista de Usuários estiver vazia, mostrar o elemento. -->
+			<template v-else>
+				<p>Não há usuários cadastrados =/</p>
+			</template>
 
 		</b-list-group>
 
@@ -80,12 +115,23 @@
 
 			return {
 
+				// Objeto que será enviado para o Banco de Dados
 				usuario: {
 					nome: '',
 					email: '',
 				},
 
-				usuarios: [],
+				// Lista de Usuários, inicia a aplicação ativando o 'método' de buscar dados no banco
+				usuarios: this.getDados(),
+
+				// Irá armazenar o ID de um usuário de dentro do Banco de Dados
+				id: null,
+
+				// Classe de estilização do 'card': true = '.registrando' ... false '.alterando'
+				classe: true,
+
+				// Array que vai receber um objeto de mensagens  
+				mensagens: [],
 
 			}
 
@@ -93,26 +139,102 @@
 
 		methods: {
 
+			// Método de Limpar 
 			limparDados(){
 				this.usuario = {}
+				this.id = null
+				this.mensagem = []
 			},
 
-			// Método e persistencia dos dados no Banco de Dados
-			salvar(){
-				// Método HTTP POST - tabela.json - objeto inserido no BD
-				this.$http.post('usuarios.json', this.usuario )
-					.then(
-						this.limparDados() // Em resposta a 'promise' chamar o método de Limpar Dados do Formulário
-					)
+			// Apagar Alerta
+			limparAlerta(){
+				setTimeout(() =>{
+					this.mensagens = []
+				}, 1500)
+			},	
+
+			// Método que seleciona um Registro dentro da Lista de Registros (BD)
+			carregar(valor){
+				// Variavel que garante que o Registro está sendo Selecionado
+				this.id = valor
+				// Adiciona o Registro selecionado dentro do Objeto Indicado (Será alterado)
+				this.usuario = {...this.usuarios[valor]}
+				this.classe = false // Atualiza estilo do card
 			},
 
-			mostrar(){
-				this.$http.get('/usuarios.json')
-					.then((resposta) => {
-						this.usuarios = resposta.data
-						
+			// Método HTTP DELETE - 'caminho/id.json' (vai receber o id que identifica qual o item que será deletado)
+			deleteDado(valor){
+				this.$http.delete(`/usuarios/${valor}.json`)
+					.then(() => {
+						// Atendendo a 'Promisses'
+						this.limparDados() // Limpar Dados do Formulário
+
+						// Mensagem de Sucesso
+						this.mensagens.push({
+							texto: 'Registro Excluído com Sucesso!',
+							tipo: 'success'
+						})
+
+						this.limparAlerta(); // Atualiza o alerta
+
+						this.getDados(); // Atualiza a lista de usuários
+						this.classe = true; // Atualiza estilo do card
+
+
+					}).catch(() => {
+						// Mensagem de Erro
+						this.mensagens.push({
+							texto: 'Não foi possivel deletar o registro!' ,
+							tipo: 'danger'
+						})
+
+						this.limparAlerta(); // Atualiza o alerta
+
 					})
+			},
+
+			// Método de persistencia e alteração dos dados no Banco de Dados
+			postEPutDados(){
+				// Condicional Ternario, indica o 'método' baseado na entrega do dado 'id'
+				const metodoHTTP = this.id ? 'patch': 'post';
+				// Condicional Ternario, indica o 'final da URL (caminho)' baseado na entrega do dado 'id'
+				const finalURL = this.id ? `/usuarios/${this.id}.json`: '/usuarios.json'
+
+				// Exemplo: axios.metodo(caminho, objeto)
+				this.$http[metodoHTTP](finalURL, this.usuario).then(() => {
+					// Atendendo a 'Promisses'
+					this.limparDados() // Limpar Dados do Formulário
+
+					// Mensagem de Sucesso
+					this.mensagens.push({
+						texto: 'Operação realizada com sucesso!',
+						tipo: 'success'
+					})
+
+					this.limparAlerta(); // Atualiza o alerta
+
+					this.getDados(); // Atualiza a lista de usuários
+					this.classe = true; // Atualiza estilo do card
+				})
+				
+			},
+
+			// Método de busca e leitura dos dados no Banco de Dados
+			getDados(){
+				
+				// Método HTTP GET - 'caminho.json'
+				this.$http.get('/usuarios.json').then((resposta) => {
+					this.usuarios = resposta.data
 					
+				}).catch(() => {
+					this.mensagens.push({
+						texto: 'Não foi possível carregar a lista!',
+						tipo: 'warning'
+					})
+
+					this.limparAlerta(); // Atualiza o alerta
+					
+				})
 			}
 		}
 
@@ -121,16 +243,31 @@
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	font-size: 1.5rem;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		font-size: 1.5rem;
+	}
 
-#app h2 {
-	text-align: center;
-	margin: 50px;
-}
+	#app h2 {
+		text-align: center;
+		margin: 50px;
+	}
+
+	#app p {
+		text-align: center;
+		margin: 50px;
+		font-size: 2rem;
+	}
+
+	.alterando {
+		background-color: #e0bf5c;;
+	}
+
+	.registrando {
+		background-color: #fff;
+	}
+
 </style>
--- servidor-http/http-exercicios/src/plugins/axios.js
+++ servidor-http/http-exercicios/src/plugins/axios.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 import Vue from 'vue';
 
 // Registrando a URL (base) do Banco de Dados (Referencia Global). // URL do Firebase
-
 // axios.defaults.baseURL = 'https://curso-vue-1130f-default-rtdb.firebaseio.com/'; 
+
+// Adicionando Headers para o uso do Axios de forma Global
+// axios.defaults.headers.common['Authorization']  = 'abc123' // Common = Toda requisição feita, mandar a chanve 'Autorization
+// axios.defaults.headers.get['Accepts'] = 'application/json' // Em requisições GET só aceitamos respostas do tipo 'application/json'
 
 Vue.use({
     install(Vue) {
@@ -16,6 +19,36 @@ Vue.use({
         Vue.prototype.$http = axios.create({
             baseURL: 'https://curso-vue-1130f-default-rtdb.firebaseio.com/',
 
+            headers: {
+                "Authorization": 'abc123',
+                "Content-Type": "application/json"
+            }
+
         })
+
+        // A ideia é interceptar 'globalmente' todas as requisições HTTP
+        Vue.prototype.$http.interceptors.request.use(requisicao => {
+            // Mostra no Console a requisição
+            console.log(requisicao.method)
+            // Config é o nome indicado na documentação
+            return requisicao
+
+        }, error => Promise.reject(error)) // Rejeitando a Promise em caso de erro
+
+        Vue.prototype.$http.interceptors.response.use(resposta => {
+            
+            // const array = []
+
+            // for(let chave in resposta.data){
+            //     array.push({id: chave, ...resposta.data[chave]}) // Forma menos manual
+            //     //array.push({id: chave, nome: resposta.data[chave].nome, email: resposta.data[chave].email}) // Forma mais manual
+            // }
+
+            // resposta.data = array
+            // console.log( resposta.data)
+            return resposta
+
+        }, error => Promise.reject(error))
+
     }
 })
